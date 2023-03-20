597197b72939014b060158ad428a4d155e453a28
Maxuk <sotskovmaxum@gmail.com>
2023-03-20 09:49:16 +0200

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function openCatalog() {
     document.getElementById("xernya").classList.toggle('active')
 }
+
+function openPudcatalog() {
+    document.getElementById("pudcatalog").classList.toggle('active')
+}
