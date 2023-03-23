aceba2f4e3c1a30f65e7c619d352e4874eb759f1
Maxuk <sotskovmaxum@gmail.com>
2023-03-23 10:14:11 +0200

    commit

--- main.js
+++
@@ -1,7 +0,0 @@
-function openCatalog() {
-    document.getElementById("xernya").classList.toggle('active')
-}
-
-function openPudcatalog() {
-    document.getElementById("pudcatalog").classList.toggle('active')
-}
