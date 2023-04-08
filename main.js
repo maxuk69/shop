38f632853a27404d30ea20ad5f059cad6fb4ce7a
Maxuk <sotskovmaxum@gmail.com>
2023-04-08 18:58:59 +0300

    addSliderAndDescription

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function openCatalog() {
 function openPudcatalog() {
     document.getElementById("pudcatalog").classList.toggle('active')
 }
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    a11y: {
+        paginationBulletMessage: 'Текст {{index}}'
+    }
+  });
