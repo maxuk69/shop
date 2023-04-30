ebb59ae5ce7ebd27a9a03f9a2025690273fcd6b3
Maxuk <sotskovmaxum@gmail.com>
2023-04-30 11:53:44 +0300

    update

--- main.js
+++ main.js
@@ -6,16 +6,13 @@ function openPudcatalog() {
     document.getElementById("pudcatalog").classList.toggle('active')
 }
 
+
 const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
     loop: true,
   
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-    a11y: {
-        paginationBulletMessage: 'Текст {{index}}'
-    }
   });
+
